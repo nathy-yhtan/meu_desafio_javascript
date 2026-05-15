18034fb7b2c67f4278b045b2eed2332c98c639c6
nathy-yhtan <nathyreismoreira@gmail.com>
2026-05-15 10:50:18 -0300

    informacoes no console log

--- js/form.js
+++ js/form.js
@@ -1,49 +1,44 @@
 
-//class contato
-
+//class contato ! TEM QUE TER
+//class pq ela simula um objeto
 class contato {
-    
+
+    constructor(nome, sobrenome, cpf, email, telefone, mensagem, contato) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+        this.cpf = cpf
+        this.email = email
+        this.telefone = telefone
+        this.mensagem = mensagem
+        this.contato = contato
+
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("cpf").value, 
+    let data = new contato(form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("cpf").value,
         form.elements.namedItem("email").value,
-            form.elements.namedItem("telefone").value, 
+        form.elements.namedItem("telefone").value,
         form.elements.namedItem("mensagem").value,
-            form.elements.namedItem("contato").value);
+        form.elements.namedItem("contato").value);
 
-            Enviar(data);
-  
-}
-
-function Enviar(data) {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+    Enviar(data);
 
 }
 
-function capturarDados(event) {
-    // Impede o formulário de recarregar a página
-    event.preventDefault();
+function Enviar(valor) {
 
-    // Seleciona todos os elementos com a classe .fordform
-    const inputs = document.querySelectorAll('.fordform');
+    document.addEventListener("submit", (e) => {
+        e.preventDefault()
+        var nome = document.getElementById("nomeid");
 
-    // Passa por cada input e mostra o valor no console
-    inputs.forEach(input => {
-        console.log(`${input.name}: ${input.value}`);
-    });
-}
+        if (nome.value != "") {
+            alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        }
+        console.log(valor)
+    }, { once: true })
 
-document.querySelectorAll('.fordform');for (let i = 0; i < document.querySelectorAll('.fordform').length; i++) {
-    document.querySelectorAll('.fordform')[i].addEventListener('input', function () {
-        console.log(this.value);
-    });
-}
+};
